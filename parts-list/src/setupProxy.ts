fe33ba786b89431c4fdedd0c2399c973e50c1462
sbbrown35 <52177069+sbbrown35@users.noreply.github.com>
2021-07-28 22:13:06 -0400

    update

--- parts-list/src/setupProxy.ts
+++
@@ -1,5 +0,0 @@
-import { createProxyMiddleware } from 'http-proxy-middleware'; 
-     
-module.exports = function(app: any) {
-    app.use(createProxyMiddleware('/**', { target: 'http://localhost:5555' }));
-};
